aa57d8178b2234d928fc7c8d434fd77ccfbe79ec
tangyou <53103447@qq.com>
2021-03-04 23:16:18 +0800

    最新修改

--- src/components/dayPayStatistics.vue
+++ src/components/dayPayStatistics.vue
@@ -4,17 +4,21 @@
       <el-col :span="1">
         收费时间
       </el-col>
-      <el-col :span="11">
+      <el-col :span="23">
         <el-date-picker v-model="searchParam.payTime" type="date" format="yyyy-MM-dd" @change="datePickerChange">
         </el-date-picker>
       </el-col>
     </el-row>
     <el-divider></el-divider>
     <div>
-      <div v-for="(value, key) in tableData.roomPayDetail" :key="key">
+      <el-card class="box-card" style="margin-bottom: 30px;" v-for="(value, key) in tableData.roomPayDetail" :key="key">
         <span>房间号：{{key}}</span>
-        <el-table :data="value" border style="width: 100%" size="small" :summary-method="getSummaries" show-summary>
-          <el-table-column prop="roomNum" label="房间号"></el-table-column>
+        <el-table :data="value" border style="width: 100%;margin-top: 10px" size="small" :row-class-name="tableRowClassName" header-row-class-name="headerClass" :summary-method="getSummaries" show-summary>
+          <el-table-column label="科目">
+            <template slot-scope="scope">
+              {{ payMode[scope.row.payMode]}}
+            </template>
+          </el-table-column>
           <el-table-column prop="customerName" label="租户"></el-table-column>
           <el-table-column prop="customerPhone" label="租户电话"></el-table-column>
           <el-table-column prop="payPrice" label="支付方式">
@@ -27,8 +31,28 @@
               </div>
             </template>
           </el-table-column>
-          <el-table-column prop="payPrice" label="总计"></el-table-column>
+          <el-table-column prop="payPrice" label="总计（元）"></el-table-column>
+          <el-table-column label="支付交易单号">
+            <template slot-scope="scope">
+              <el-input v-model="scope.row.transactionNo" placeholder="支付交易单号" ></el-input>
+            </template>
+          </el-table-column>
         </el-table>
+      </el-card>
+      <div>
+        <el-card class="box-card" style="width:25%;margin-right:30px;margin-bottom:20px;float: right">
+          <el-row type="flex" justify="end" align="middle" style="padding: 5px;font-size: 22px;font-weight: 800">
+            <el-col :span="10">支付方式</el-col>
+            <el-col :span="14">总计金额（元）</el-col>
+          </el-row>
+          <el-row type="flex" justify="end" align="middle" v-for="(value, key) in tableData.feeMap" :key="key" style="padding: 5px;;font-size: 18px;font-weight: 600">
+            <el-col :span="10">{{payType[key]}}</el-col>
+            <el-col :span="14">{{value}}</el-col>
+          </el-row>
+          <el-row type="flex" justify="center" align="middle">
+            <el-col :span="24"><el-button type="primary">确认无误，下一步</el-button></el-col>
+          </el-row>
+        </el-card>
       </div>
     </div>
   </div>
@@ -84,11 +108,34 @@
           }
         });
         return sums;
+      },
+      tableRowClassName:function(row, rowIndex){
+        row.toString()
+        if (rowIndex % 2 === 0) {
+          return 'warning-row';
+        }else{
+          return 'success-row'
+        }
       }
     }
   }
 </script>
 
 <style scoped>
-
+  .el-table .headerClass{
+    color: black;
+    font-weight: 800;
+    font-size: 18px;
+  }
+  .el-table .warning-row {
+    background: #f6f6f6;
+    color: black;
+    font-weight: 400;
+    font-size: 16px;
+  }
+  .el-table .success-row {
+    color: black;
+    font-weight: 400;
+    font-size: 16px;
+  }
 </style>
